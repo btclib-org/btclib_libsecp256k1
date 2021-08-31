a1341f1927eb7ea81f685f5027029c39a3b087f8
Giacomo Caironi <giacomo.caironi@gmail.com>
2021-08-31 21:19:00 +0200

    Improved function description

--- build.py
+++ build.py
@@ -1,32 +1,51 @@
 import cffi
-import glob
+import pathlib
+import sys
 import os
+import re
+import glob
+import subprocess
+from subprocess import Popen, PIPE
 
-for file in glob.glob("_btclib_libsecp256k1.*"):
-    os.remove(file)
+on_win = sys.platform.startswith("win")
+
+"""Remove any built objects"""
+patterns = ("*.o", "*.so", "*.obj", "*.dll", "*.exp", "*.lib", "cffi_example*")
+for file_pattern in patterns:
+    for file in glob.glob(file_pattern):
+        os.remove(file)
+
+subprocess.call("./autogen.sh".split(), cwd="secp256k1")
+command = "./configure --disable-tests --disable-benchmark --enable-experimental --enable-module-schnorrsig"
+subprocess.call(command.split(), cwd="secp256k1")
+subprocess.call("make".split(), cwd="secp256k1")
 
 ffi = cffi.FFI()
 
-code = """
-typedef struct secp256k1_context_struct secp256k1_context;
+headers = ["secp256k1_schnorrsig.h"]
 
-secp256k1_context* secp256k1_context_create(
-    unsigned int flags
-);
-"""
-ffi.cdef(code)
+ffi_header = ""
+for h in headers:
+    ffi_header += f'#include "secp256k1/include/{h}"' + "\n"
+
+command = "gcc -P -E -".split()
+p = Popen(command, stdin=PIPE, stdout=PIPE)
+definitions = p.communicate(input=ffi_header.encode())[0].decode()
+definitions = re.sub(" __.*;", ";", definitions)
+definitions = re.sub("__.*\)\) ", "", definitions)
+definitions = "\n".join(definitions.splitlines()[7:])
+ffi.cdef(definitions)
+
 
 ffi.set_source(
-    "_btclib_libsecp256k1",
-    '#include "secp256k1.h"',
-    extra_objects=["libsecp256k1.so.0"],
+    "btclib_libsecp256k1",
+    ffi_header,
+    extra_objects=["secp256k1/.libs/libsecp256k1.so"],
     # include_dirs=[include_dir.as_posix()],
     # libraries=["secp256k1"],
-    # library_dirs=[shared_dir.as_posix()]
+    # library_dirs=["secp256k1/.libs/"],
     # libraries=["stddef"],
-    extra_link_args=["-Wl,-rpath,."],
+    extra_link_args=["-Wl,-rpath,secp256k1/.libs/"],
 )
 
 ffi.compile(verbose=True)
-
-import _btclib_libsecp256k1
--- ctype.py
+++
@@ -1,6 +0,0 @@
-# ctypes_test.py
-import ctypes
-import pathlib
-
-libname = pathlib.Path().absolute() / "secp256k1" / ".libs" / "libsecp256k1.so"
-c_lib = ctypes.CDLL(libname)

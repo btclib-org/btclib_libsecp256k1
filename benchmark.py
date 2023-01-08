76d6025b0cdc3ffe9167f1ac3e4a160d45733db7
Giacomo Caironi <giacomo.caironi@gmail.com>
2023-01-08 23:42:24 +0100

    Add secp256k1-py and coincurve ssa to benchmarks

--- benchmark.py
+++ benchmark.py
@@ -18,9 +18,11 @@ from btclib.to_pub_key import pub_keyinfo_from_prv_key  # type: ignore
 
 import btclib_libsecp256k1.dsa
 import btclib_libsecp256k1.ssa
+import secp256k1
 
 prvkey = 1
 pubkey = pub_keyinfo_from_prv_key(prvkey)[0]
+xonly_pubkey = pubkey[1:]
 msg = reduce_to_hlen(b"Satoshi Nakamoto")
 dsa_sig = btclib_libsecp256k1.dsa.sign(msg, prvkey)
 ssa_sig = btclib_libsecp256k1.ssa.sign(msg, prvkey)
@@ -42,24 +44,44 @@ def dsa_coincurve() -> None:
     assert coincurve.PublicKey(pubkey).verify(dsa_sig, msg, None)  # nosec B101
 
 
+def ssa_coincurve() -> None:
+    assert coincurve.PublicKeyXOnly(xonly_pubkey).verify(ssa_sig, msg)  # nosec B101
+
+
+def dsa_secp256k1():
+    pubkey_secp = secp256k1.PublicKey(pubkey, raw=True)
+    assert pubkey_secp.ecdsa_verify(
+        msg, pubkey_secp.ecdsa_deserialize(dsa_sig), raw=True
+    )
+
+
+def ssa_secp256k1():
+    pubkey_secp = secp256k1.PublicKey(pubkey, raw=True)
+    assert pubkey_secp.schnorr_verify(msg, ssa_sig, None, raw=True)
+
+
 def dsa_libsecp256k1() -> None:
     assert btclib_libsecp256k1.dsa.verify(msg, pubkey, dsa_sig)  # nosec B101
 
 
 def ssa_libsecp256k1() -> None:
-    assert btclib_libsecp256k1.ssa.verify(msg, pubkey, ssa_sig)  # nosec B101
+    assert btclib_libsecp256k1.ssa.verify(msg, xonly_pubkey, ssa_sig)  # nosec B101
 
 
 def benchmark(func: Callable[[], None], mult: int = 1) -> None:
     start = time.time()
-    for _ in range(100 * mult):
+    for _ in range(1000 * mult):
         func()
     end = time.time()
     print(f"{func.__name__:<17}: {((end - start) / mult):.6f}")
 
 
-benchmark(dsa_btclib, 100)
+benchmark(dsa_btclib, 10)
 benchmark(dsa_coincurve, 100)
+benchmark(dsa_secp256k1, 100)
 benchmark(dsa_libsecp256k1, 100)
-benchmark(ssa_btclib, 100)
+
+benchmark(ssa_btclib, 10)
+benchmark(ssa_coincurve, 100)
+benchmark(ssa_secp256k1, 100)
 benchmark(ssa_libsecp256k1, 100)
